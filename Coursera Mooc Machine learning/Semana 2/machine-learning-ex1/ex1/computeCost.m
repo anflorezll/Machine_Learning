82dcdf5e2ed451e2cc5301390c2a30b461dd9281
jsrestrepom <santyrestrepom@gmail.com>
2016-07-17 12:00:27 -0500

    Se coloca el codigo en la seccion correspondiente.

--- Coursera Mooc Machine learning/Semana 2/machine-learning-ex1/ex1/computeCost.m
+++ Coursera Mooc Machine learning/Semana 2/machine-learning-ex1/ex1/computeCost.m
@@ -9,15 +9,12 @@ m = length(y); % number of training examples
 % You need to return the following variables correctly 
 J = 0;
 
-J = sum(((X*theta) - y).^2);
-J = 1/(2*m)*J ;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+J = sum(((X*theta) - y).^2);
+J = 1/(2*m)*J ;
 
 % =========================================================================
 
