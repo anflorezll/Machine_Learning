a4b51d9a77cfcb6d60bac246bcf30054819d5d33
Andres Florez Llano <Andres Florez Llano>
2016-07-21 00:04:45 -0500

    la primera parte de los ex2, estoy teniendo problemas con el tamaño de las matrices o no se en que me estoy equivocando y va mas alla de que solo compute el numero pues aun cuando modifique para que me diera un resultado no parecia ser el correcto igual dire que no lo termine de corregir para que tubieramos algo para hacer en la reunion

--- Coursera Mooc Machine learning/Semana 3/machine-learning-ex2/ex2/costFunction.m
+++ Coursera Mooc Machine learning/Semana 3/machine-learning-ex2/ex2/costFunction.m
@@ -1,5 +1,5 @@
 function [J, grad] = costFunction(theta, X, y)
-%COSTFUNCTION Compute cost and gradient for logistic regression
+% COSTFUNCTION Compute cost and gradient for logistic regression
 %   J = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
 %   parameter for logistic regression and the gradient of the cost
 %   w.r.t. to the parameters.
@@ -8,8 +8,16 @@ function [J, grad] = costFunction(theta, X, y)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
+J = (1/m)*sum(-y*log(sigmoid(theta'*X))-(1-y)*log(1-sigmoid(theta'*X))); % Mi primer intento, 
+                                                           % para computar la funcion de costos
+                                                           % el detalle creo que esta en el theta'*x 
+                                                           % usado en la funcion sigmoid ya definida g()
+
 grad = zeros(size(theta));
+
+grad = (1/m)*sum(sigmoid(theta'*X)-y)*X ; % defino el gradiente nuevamente utilizando  
+                                  % la funcion de sigmoid,tengo la duda es conrespecto
+                                  % a que son 2 X y no se como eso lo ejecuta 
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -18,15 +26,5 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 %
 % Note: grad should have the same dimensions as theta
-%
-
-
-
-
-
-
-
-
-% =============================================================
 
 end
--- Coursera Mooc Machine learning/Semana 3/machine-learning-ex2/ex2/sigmoid.m
+++ Coursera Mooc Machine learning/Semana 3/machine-learning-ex2/ex2/sigmoid.m
@@ -5,7 +5,6 @@ function g = sigmoid(z)
 % You need to return the following variables correctly 
 g = zeros(size(z));
 
-g = 1/(1+ exp(-z)) % funcion sigmoid para los valores en z= theta'* X
-
+g = 1/(1+ exp(-z)); % funcion sigmoid para los valores en z= theta'* X
 
 end
